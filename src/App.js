a33f74dd2962ad3764e34f4ec25a349b4b46c3ea
Ansah1234 <ansahpeter123@yaoo.com>
2021-03-12 16:29:49 +0100

    clearing biolerplate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="App">
+	
+		</div>
+	);
+};
 
 export default App;
